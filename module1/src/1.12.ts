5d90638bd3ed6ceb30e4b463ae5df885a2fbee19
Siftab <siiffuuu@gmail.com>
2024-05-02 08:57:36 +0600

    first typescript code

--- module1/src/1.12.ts
+++ module1/src/1.12.ts
@@ -1,8 +1,22 @@
-const searchName = (value: string) => {
-  if (value) {
-    console.log("searching ");
+// const searchName = (value: string) => {
+//   if (value) {
+//     console.log("searching ");
+//   } else {
+//     console.log("nothing to search");
+//   }
+// };
+// searchName("good");
+const getSpeedMeterPerSecond = (value: unknown) => {
+  if (typeof value === "number") {
+    const convertedSpeed: number = (value * 1000) / 3600;
+    console.log(convertedSpeed);
+  }
+  if (typeof value === "string") {
+    const targetValue: string = value.split(" ")[0];
+    const convertedSpeed: number = (parseFloat(targetValue) * 1000) / 3600;
+    console.log(convertedSpeed);
   } else {
-    console.log("nothing to search");
+    console.log("nothing to claculate");
   }
 };
-searchName(null);
+getSpeedMeterPerSecond("10000 is");
