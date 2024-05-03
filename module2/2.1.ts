8c59c297a5a13511b05f2b442521c21d9cc1f8e0
Siftab <siiffuuu@gmail.com>
2024-05-03 12:00:56 +0600

    assertion

--- module2/2.1.ts
+++ module2/2.1.ts
@@ -4,5 +4,20 @@
     console.log(typeof (anyThing as number)
     )
     // anyThing.
+    // type demo = (x: string | number) => any
+    const kgTOGram = (value: string | number) => {
+        if (typeof value === "string") {
+            return ` stringi value ${parseFloat(value) * 1000}`
+        }
+        if (typeof value === "number") {
+            return value * 1000
+
+        }
+
+    }
+
+    const result1 = kgTOGram("1000") as string
+    const result2 = kgTOGram(1000) as number
+    console.log(result1)
 }
 
