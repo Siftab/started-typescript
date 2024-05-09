b305b2504492bd4b6fb64b6ec2779d018d3dc0f4
Siftab <siiffuuu@gmail.com>
2024-05-09 12:01:33 +0600

    add

--- module2/2.10.ts
+++ module2/2.10.ts
@@ -2,7 +2,7 @@
     const numbers: number[] = [1, 2, 3, 4, 5]
 
     const stringNumbers: string[] = numbers.map(number => number.toString())
-    // console.log(stringNumbers)
+    console.log(stringNumbers)
 
     type arryDev = {
         forntend: string;
@@ -11,5 +11,18 @@
     type newarry = {
         [key in keyof arryDev]: boolean
     }
+    type areaString = {
+        height: string,
+        width: number
+    }
+    type newAreaString<T> = {
+        [key in keyof T]: T[key]
+    }
+
+    const valuetop: newAreaString<areaString> = {
+        height: "90",
+        width: 99
+    }
+
 
 }
