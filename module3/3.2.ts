76ed6750b4d82084db98561164e1cdc59ddeb98a
Siftab <siiffuuu@gmail.com>
2024-05-13 07:59:58 +0600

    add

--- module3/3.2.ts
+++ module3/3.2.ts
@@ -3,14 +3,27 @@
     // oop=inharitance 
     class student {
 
-        constructor(public name: string, age: number, public adress: string) {
+        constructor(public name: string, public age: number, public adress: string) {
 
         }
         getSleep() {
-            console.log(`${this.name} is sleeping `)
+            console.log(`${this.name} is sleeping and age is ${this.age}`)
+        }
+    }
+    class teacher extends student {
+        constructor(name: string, age: number, address: string, public designation: string) {
+            super(name, age, address)
+
+
+        }
+        teaching() {
+            console.log(`${this.name} is a ${this.designation} and age${this.age}`)
         }
     }
 
+    const teacher2 = new teacher("korim", 55, "dhaka", "president")
     const student1 = new student("sifat", 77, "keranigonj")
     student1.getSleep();
+    teacher2.getSleep();
+    teacher2.teaching()
 }
